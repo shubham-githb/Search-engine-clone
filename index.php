501c39d4aa0bc3ff1d1727f5bcfd4cb06eed985f
shubham-githb <72751187+shubham-githb@users.noreply.github.com>
2021-01-24 14:34:18 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to theBartSearch</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+    <style>
+
+    body{
+        background-image: url('VEC.jpg');
+        background-image: no-repeat;
+        background-size: 100%;
+        height: 100%;
+        background-position: center;
+        background-size: conver;
+    }
+
+    </style>
+<body>
+
+ <div class = "wrapper IndexPage">
+
+    <div class ="mainSection">
+
+        <div class="logoContainer">
+            <img src="log.png">
+        </div>
+
+        <div class="searchContainer">
+
+            <form action="search.php" method="GET">
+
+                <input class="searchBox" type="text" name="term" placeholder="Enter your search here....">
+                <input class="searchButton" type="submit" value="Search">
+
+            </form>
+
+        </div>
+    
+    </div>
+
+</div>
+
+
+</body>
+</html>
---
+++ search.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to theBartSearch</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+    <style>
+    body{
+        background-image: url('1876.jpg');
+        background-image: no-repeat;
+        background-size: 100%;
+        height: 100%;
+        background-position: center;
+        background-size: conver;
+    }
+    </style>
+
+<body>
+
+ <div class = "wrapper">
+
+     <div class="header">
+
+        <div class = "headerContent">
+
+            <div class="logoContainer">
+                <a href = "index.php">
+                <img src="log.png"  alt="logo">
+                </a>
+            </div>
+
+            <div class="searchContainer">
+
+                <form action="search.php" method=get>
+
+                    <div class="searchBarContainer">
+
+                        <input class="searchBox" type="text" name="term">
+                        <button >
+                            search
+                        </button>
+
+                </form>
+
+            </div>
+
+        </div>
+
+    </div>    
+</div>
+
+
+</body>
+</html>
